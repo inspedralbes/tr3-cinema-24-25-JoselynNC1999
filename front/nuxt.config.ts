59cae559f7ecbff50c16c7f7df841e91edc2516a
a23josnincal <a23josnincal@inspedralbes.cat>
2025-03-12 09:05:18 +0100

    feat:instalar pinia

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // Asegúrate de tener configurado el módulo @nuxtjs/tailwindcss
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@pinia/nuxt","@nuxtjs/tailwindcss"],
 
   // (Opcional) Si quieres personalizar tu archivo CSS principal
   css: ['@/assets/css/main.css'],
